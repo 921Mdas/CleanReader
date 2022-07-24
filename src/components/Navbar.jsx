cd767b1ee82f230bbeac32b6023d3516b8102730
Deo G <66809588+921Mdas@users.noreply.github.com>
2022-07-24 14:04:51 -0400

    visual cleanup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "./components.scss";
+import { BsFillGearFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="nav_sec1">toggles</div>
-      <div className="nav_sec2">iconStyle</div>
-      <div className="nav_sec3">Exit</div>
+      <div className="nav_sec1"></div>
+      <div className="nav_sec2">
+        <div className="nav_subsec1">
+          <p>Input</p>
+          <h3>CSV</h3>
+        </div>
+        <div className="convert_icon">
+          <BsFillGearFill color="white" className="convert_icon_el" />
+        </div>
+        <div className="nav_subsec2">
+          <p>Output</p>
+          <h3>TXT</h3>
+        </div>
+      </div>
+      <div className="nav_sec3"></div>
     </div>
   );
 };
